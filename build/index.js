29f286be9de6c77962d5888b48f88ec436c55b4f
Yuichi Hagio <yhagio87@gmail.com>
2016-04-02 15:22:07 -0400

    Simple project list and detail page mock

--- build/index.js
+++ build/index.js
@@ -4,6 +4,18 @@ var _express = require('express');
 
 var _express2 = _interopRequireDefault(_express);
 
+var _path = require('path');
+
+var _path2 = _interopRequireDefault(_path);
+
+var _expressHandlebars = require('express-handlebars');
+
+var _expressHandlebars2 = _interopRequireDefault(_expressHandlebars);
+
+var _bodyParser = require('body-parser');
+
+var _bodyParser2 = _interopRequireDefault(_bodyParser);
+
 var _morgan = require('morgan');
 
 var _morgan2 = _interopRequireDefault(_morgan);
@@ -17,11 +29,25 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 var app = (0, _express2.default)();
 
 
+app.set('views', _path2.default.join(__dirname, '../views'));
+app.engine('handlebars', (0, _expressHandlebars2.default)({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+app.use(_bodyParser2.default.json());
+app.use(_bodyParser2.default.urlencoded({ extended: false }));
+app.use(_express2.default.static('public'));
 app.use((0, _morgan2.default)('combined'));
 app.use((0, _helmet2.default)());
 
 app.route('/').get(function (req, res) {
-  res.send({ 'greet': 'Hello' });
+  res.render('home');
+}).post(function (req, res) {
+  console.log(req.body);
+});
+
+var projects = [{ name: 'Hello' }, { name: 'Hola' }, { name: 'Bonjour' }, { name: 'Bon dia' }, { name: 'Konnichiwa' }];
+
+app.route('/projects').get(function (req, res) {
+  res.render('projects/project-list', { projects: projects });
 }).post(function (req, res) {
   console.log(req.body);
 });
--- src/index.js
+++ src/index.js
@@ -1,14 +1,39 @@
 import express from 'express';
 const app = express();
+import path from 'path';
+import exphbs from 'express-handlebars';
+import bodyParser from 'body-parser';
 import morgan from 'morgan';
 import helmet from 'helmet';
 
+app.set('views', path.join(__dirname, '../views'));
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static('public'));
 app.use(morgan('combined'));
 app.use(helmet());
 
 app.route('/')
   .get((req, res) => {
-    res.send({'greet':'Hello'});
+    res.render('home');
+  })
+  .post((req, res) => {
+    console.log(req.body);
+  });
+
+let projects = [
+  { name: 'Hello'},
+  { name: 'Hola'},
+  { name: 'Bonjour'},
+  { name: 'Bon dia'},
+  { name: 'Konnichiwa'}
+];
+
+app.route('/projects')
+  .get((req, res) => {
+    res.render('projects/project-list', {projects: projects});
   })
   .post((req, res) => {
     console.log(req.body);
