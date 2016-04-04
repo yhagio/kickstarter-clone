e30c8ad31931038a3eddbaf874e07ed5eeb510c7
Yuichi Hagio <yhagio87@gmail.com>
2016-04-04 07:37:37 -0400

    Added more routes and static views

--- src/routes.js
+++ src/routes.js
@@ -3,15 +3,31 @@ import projectHandler from './handlers/projects';
 export default (app) => {
   // Home
   app.route('/')
-    .get((req, res) => {
-      res.render('home');
-    });
+    .get((req, res) => res.render('home'));
+
+  // Home
+  app.route('/about')
+    .get((req, res) => res.render('about'));
 
   // Sign up
   app.route('/signup')
-    .get((req, res) => {
-      res.render('authentication/signup');
-    });
+    .get((req, res) => res.render('authentication/signup'));
+
+  // Log in
+  app.route('/login')
+    .get((req, res) => res.render('authentication/login'));
+
+  // Forgot password
+  app.route('/forgot-password')
+    .get((req, res) => res.render('authentication/forgot-password'));
+
+  // Terms of Use
+  app.route('/terms-of-use')
+    .get((req, res) => res.render('authentication/terms-of-use'));
+
+  // Privacy Policy
+  app.route('/privacy')
+    .get((req, res) => res.render('authentication/privacy'));
 
   // Project Lit
   app.route('/projects')
@@ -23,8 +39,6 @@ export default (app) => {
 
   // Create Project Page
   app.route('/create-project')
-    .get((req, res) => {
-      res.render('projects/project-create');
-    })
+    .get((req, res) => res.render('projects/project-create'))
     .post(projectHandler.postProjectCreate);
 }
